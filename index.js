bba324f09494b62f5b768ea2c68b0c3db0f53631
tunghiu <raptor2377pun0suy@hotmail.com>
2024-05-09 11:43:30 +0700

    feat task

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-function isAnagram(s, t) {
-  if (s.length !== t.length) return false;
-  const map = new Map();
-  for (const char of s) {
-    map.set(char, (map.get(char) || 0) + 1);
-  }
-  for (const char of t) {
-    if (!map.has(char) || map.get(char) === 0) return false;
-    map.set(char, map.get(char) - 1);
-  }
-  return true;
-}
+const name = "john";
+const upperName = name.toUpperCase();
+console.log(upperName); // JOHN
