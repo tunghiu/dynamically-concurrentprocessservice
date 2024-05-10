e4a9f4e43f2f4478298f78ee3fc699b0337f8193
tunghiu <raptor2377pun0suy@hotmail.com>
2024-05-10 10:42:07 +0700

    update task

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const name = "john";
-const upperName = name.toUpperCase();
-console.log(upperName); // JOHN
+const pullAtIndex = (arr, pullArr) => {
+  let removed = [];
+  let pulled = arr
+    .map((v, i) => (pullArr.includes(i) ? removed.push(v) : v))
+    .filter((v, i) => !pullArr.includes(i));
+  arr.length = 0;
+  pulled.forEach((v) => arr.push(v));
+  return removed;
+};
