f45521f5e761caf61eb8be04964f686bd4e0a5e8
tunghiu <raptor2377pun0suy@hotmail.com>
2024-05-13 20:16:01 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const mergeSort = (arr) => {
-  if (arr.length <= 1) {
-    return arr;
-  }
-  const mid = Math.floor(arr.length / 2);
-  const left = mergeSort(arr.slice(0, mid));
-  const right = mergeSort(arr.slice(mid));
-  return merge(left, right);
-};
-const merge = (left, right) => {
-  let result = [];
-  let leftIndex = 0;
-  let rightIndex = 0;
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      result.push(left[leftIndex]);
-      leftIndex++;
-    } else {
-      result.push(right[rightIndex]);
-      rightIndex++;
-    }
-  }
-  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-};
+function findNumbers(nums) {
+  return nums.filter((num) => num.toString().length % 2 === 0).length;
+}
