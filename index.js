7c5800b7af2f156b6225f76d0cdcfb9e139d4ed4
tunghiu <raptor2377pun0suy@hotmail.com>
2024-05-14 12:00:43 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function findNumbers(nums) {
-  return nums.filter((num) => num.toString().length % 2 === 0).length;
+function mergeKLists(lists) {
+  if (!lists || !lists.length) return null;
+  return mergeLists(lists, 0, lists.length - 1);
+  function mergeLists(lists, start, end) {
+    if (start === end) return lists[start];
+    const mid = Math.floor((start + end) / 2);
+    const left = mergeLists(lists, start, mid);
+    const right = mergeLists(lists, mid + 1, end);
+    return mergeTwoLists(left, right);
+  }
+  function mergeTwoLists(l1, l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+    if (l1.val < l2.val) {
+      l1.next = mergeTwoLists(l1.next, l2);
+      return l1;
+    } else {
+      l2.next = mergeTwoLists(l1, l2.next);
+      return l2;
+    }
+  }
 }
